a42c69e5ca62fceafe019d48c2e6e0d6204dab2e
Vikas <=>
2015-02-03 12:48:37 +0530

    version 2 done

--- rpn.c
+++ rpn.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+// #define atoa(x) #x
 
 
 int operate(char operator, string operand1, string operand2){
@@ -16,11 +17,29 @@ int operate(char operator, string operand1, string operand2){
 	return result ;
 }
 
+char *toString (int value, char *result, int base){
+    char *ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+    while (value){
+        tmp_value = value; value /= base;
+        *ptr++ = "9876543210123456789" [9 + (tmp_value - value * base)];
+    };
+    tmp_value < 0 && (*ptr++ = '-');
+    *ptr-- = '\0';
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
+
 
 int evaluate(char *expr){
-	Stack stack = createStack(); char seperator = ' ', operator;
+	Stack stack = createStack(); char seperator = ' ', operator, *returnValue, *result;
 	string number = calloc(1, sizeof(char)), operand1, operand2;
-	int i, j=0, value, result, *returnValue;
+	int i, j=0, k, value;
 	
 	for(i=0,j=0; i<strlen(expr); i++){
 		if(expr[i] <=57 && expr[i] >=48 ){
@@ -28,16 +47,16 @@ int evaluate(char *expr){
 			number[j] = expr[i]; j++;
 		} 
 		if(expr[i] == seperator && (number[0] >=48 && number[0] <=57)){
-				push(&stack, number);
-				number = (char*)calloc(1, sizeof(char));  j=0;
+				push(&stack, number);  j=0;
+				number = (char*)calloc(1, sizeof(char)); 
 		}
 		if(expr[i] == 42 || expr[i] == 43 || expr[i] == 45 || expr[i] == 47){
-			operand2 = pop(&stack); 
-			operand1 = pop(&stack);
-			returnValue = (int*)malloc(sizeof(int));
-			*returnValue =  operate(expr[i], operand1, operand2);
+			operand2 = pop(&stack); operand1 = pop(&stack);
+			returnValue = (string)malloc(sizeof(char)*10);
+			returnValue = toString(operate(expr[i], operand1, operand2), returnValue, 10);
 			push(&stack, returnValue);
 		}
 	}
-	return *(int*)stack.top->data;
+	free(number); free(stack.list);
+	return atoi((string)stack.top->data);
 }
--- rpnTest.c
+++ rpnTest.c
@@ -32,9 +32,24 @@ void test_evaluate_returns_the_result_of_postfix_subtraction_two_digits_minus_1_
 	assertEqual(evaluate(expression), -2);
 }
 
-void test_evaluate_returns_the_result_of_postfix_three_digits_addition_15_for_12_and_3(){
+void test_evaluate_returns_the_result_of_postfix_multiplication_of_two_digits_144_for_12_and_12(){
+	string expression = "12 12 *";
+	assertEqual(evaluate(expression), 144);
+}
+
+void test_evaluate_returns_the_result_of_postfix_division_of_two_digits_7_for_84_and_12(){
+	string expression = "84 12 /";
+	assertEqual(evaluate(expression), 7);
+}
+
+void test_evaluate_returns_the_result_of_postfix_three_digits_addition_465_for_123_and_342(){
 	string expression = "123 342 +";
 	assertEqual(evaluate(expression), 465);
+}
+
+void test_evaluate_returns_the_result_of_postfix_three_digits_division_80_for_960_and_12(){
+	string expression = "960 12 /";
+	assertEqual(evaluate(expression), 80);
 }
 
 void test_evaluate_returns_the_result_of_postfix_four_digits_addition_15_for_12_and_3(){
@@ -42,12 +57,52 @@ void test_evaluate_returns_the_result_of_postfix_four_digits_addition_15_for_12_
 	assertEqual(evaluate(expression), 5555);
 }
 
+void test_evaluate_returns_the_result_of_postfix_four_digits_subtraction_minus_3087_for_1234_and_4321(){
+	string expression = "1234 4321 -";
+	assertEqual(evaluate(expression), -3087);
+}
+
 void test_evaluate_returns_the_result_of_postfix_five_digits_addition_15_for_12_and_3(){
 	string expression = "12734 43827 +";
 	assertEqual(evaluate(expression), 56561);
 }
 
-// void test_evaluate_returns_the_result_of_postfix_expression_for_2_3_and_4(){
-// 	string expression = "2 3 4 + -";
-// 	assertEqual(evaluate(expression), -5);
+void test_evaluate_returns_the_result_of_postfix_five_digits_division_by_3_digit_number(){
+	string expression = "27183 123 /";
+	assertEqual(evaluate(expression), 221);
+}
+
+void test_evaluate_returns_the_result_of_postfix_expression_for_2_3_and_4(){
+	string expression = "2 3 4 + -";
+	assertEqual(evaluate(expression), -5);
+}
+
+void test_evaluate_returns_the_result_of_postfix_expression_for_22_3_and_4(){
+	string expression = "22 3 4 * + ";
+	assertEqual(evaluate(expression), 34);
+}
+
+void test_evaluate_returns_the_result_of_postfix_complex_multiplication_of_1_2_3_4_5(){
+	string expression = "1 2 3 4 5 * * * * ";
+	assertEqual(evaluate(expression), 120);
+}
+
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_3(){
+	string expression = "2 2 2 * 2 - 3 + + ";
+	assertEqual(evaluate(expression), 7);
+}
+
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_2_2_2(){
+	string expression = "2 2 2 2 2 * * 2 + + 2 - *";
+	assertEqual(evaluate(expression), 20);
+}
+
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_2_2(){
+	string expression = "2 2 - 2 2 2 * 2 - - -";
+	assertEqual(evaluate(expression), 0);
+}
+
+// void test_evaluate_returns_the_result_of_postfix_complex_expression_of_5_1_2_4_3(){
+// 	string expression = "5 1 2 + 4 × + 3 −"; printf("%s\n",expression);
+// 	assertEqual(evaluate(expression), 14);
 // }
